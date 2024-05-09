e3d145c4f780aaefd26c6bce1a179eb11792505e
codekaitw <65647662+codekaitw@users.noreply.github.com>
2024-05-09 17:46:25 -0400

    completed ex 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,25 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    if (string.length === 0) {
+        return '';
+    }
+    if (string.length === 1) {
+        return string;
+    }
+    return reverseString(string.slice(1)) + string[0];
 };
+
+// Solution using loop
+// const reverseStringLoop = function(string) {
+//     if (string.length === 0) {
+//         return '';
+//     }
+//     let result = '';
+//     for (let i = string.length - 1; i >= 0; i--) {
+//         result += string[i];
+//     }
+//     return result;
+// }
+
 
 // Do not edit below this line
 module.exports = reverseString;

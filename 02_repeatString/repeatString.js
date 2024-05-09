a4e69591e847b7671f94f61caef78e05e2400b2a
codekaitw <65647662+codekaitw@users.noreply.github.com>
2024-05-09 17:15:02 -0400

    completed ex 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    if (num === 0) {
+        return '';
+    }
+    if (num === 1) return string;
 
+    if (num >= 2) return string + repeatString(string, num - 1);
 };
+
+// Solution using a loop
+const repeatStringLoop = function(string, num) {
+    if (num < 0) return "ERROR";
+    let result = "";
+    for (let i = 0; i < num; i++) {
+        result += string;
+    }
+    return result;
+}
 
 // Do not edit below this line
 module.exports = repeatString;

d33cea78f57c6a4b22ea09b8d76e327dfcc5392e
codekaitw <65647662+codekaitw@users.noreply.github.com>
2024-05-09 18:28:33 -0400

    completed ex 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,30 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for (let word of args) {
+        while (arr.includes(word)) {
+            let index = arr.indexOf(word);
+            if (index > -1) {
+                arr.splice(index, 1);
+            }
+        }
+    }
+    return arr;
 };
+
+// // Odin solution 01
+// const removeFromArray = function (array, ...args) {
+//     const newArray = [];
+//     array.forEach((item) => {
+//         if(!args.includes(item)) {
+//             newArray.push(item);
+//         }
+//     });
+//     return newArray;
+// }
+
+// // Odin solution 02
+// const removeFromArray = function (array, ...args) {
+//     return array.filter(item => !args.includes(item));
+// }
 
 // Do not edit below this line
 module.exports = removeFromArray;
